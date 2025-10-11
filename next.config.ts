5bd80545c58d15770071bdda980c9895ce820aa6
Masaki Hara <ackie.h.gmai@gmail.com>
2025-10-11 17:36:36 +0900

    Configure outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/.*": ["./public/data/**/*"],
+  },
 };
 
 export default nextConfig;
