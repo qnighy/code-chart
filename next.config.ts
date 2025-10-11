353c9bfbc0afb61600cca90b3741051f1c0d8da7
Masaki Hara <ackie.h.gmai@gmail.com>
2025-10-11 17:39:39 +0900

    Fix outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/.*": ["./public/data/**/*"],
+    "/u/\\[codepoint\\]": ["./public/data/**/*"],
   },
 };
 
