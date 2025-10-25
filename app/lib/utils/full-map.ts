deecee4ff9b90087fb1c26f78c04930fea63a486
Masaki Hara <ackie.h.gmai@gmail.com>
2025-10-25 10:43:06 +0900

    Ensure exhaustiveness

--- app/lib/utils/full-map.ts
+++ app/lib/utils/full-map.ts
@@ -1,11 +1,14 @@
-export interface FullMap<K, V> extends ReadonlyMap<K, V> {
+export interface FullMap<in out K, V> extends ReadonlyMap<K, V> {
   get(key: K): V;
 }
 
 export interface FullMapConstructor {
   new <V, const Entries extends readonly (readonly [unknown, V])[]>(
     entries: Entries,
-  ): FullMap<Entries extends readonly (readonly [infer K, V])[] ? K : never, V>;
+  ): FullMap<
+    NoInfer<Entries> extends readonly (readonly [infer K, V])[] ? K : never,
+    V
+  >;
 }
 
 export const FullMap: FullMapConstructor = Map as FullMapConstructor;
